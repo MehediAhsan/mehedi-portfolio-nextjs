1bd8c75b5f032b03c509a59eabf5eab8fac7b44f
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-04-02 20:40:41 +0600

    added skills data

---
+++ src/data/skillsData.js
@@ -0,0 +1,134 @@
+import html from "../assets/skillsLogo/html-5.png";
+import css from "../assets/skillsLogo/css-3.png";
+import js from "../assets/skillsLogo/js.png";
+import react from "../assets/skillsLogo/react.png";
+import node from "../assets/skillsLogo/node-js.png";
+import mongodb from "../assets/skillsLogo/mongodb.png";
+import tailwind from "../assets/skillsLogo/tailwind-css.png";
+import express from "../assets/skillsLogo/express-js.png";
+import firebase from "../assets/skillsLogo/firebase.png";
+import bootstrap from "../assets/skillsLogo/bootstrap.png";
+import typescript from "../assets/skillsLogo/typescript.png";
+import nextjs from "../assets/skillsLogo/nextjs.png";
+import vscode from "../assets/skillsLogo/vscode.png";
+import postman from "../assets/skillsLogo/postman.png";
+import figma from "../assets/skillsLogo/figma.png";
+import illustrator from "../assets/skillsLogo/illustrator.png";
+import git from "../assets/skillsLogo/git.png";
+
+export const skills = {
+    expert: [
+      {
+        id: "1",
+        name: "html",
+        img: html,
+        percentage: "90%",
+      },
+      {
+        id: "2",
+        name: "css",
+        img: css,
+        percentage: "90%",
+      },
+      {
+        id: "3",
+        name: "javascript",
+        img: js,
+        percentage: "80%",
+      },
+      {
+        id: "4",
+        name: "react",
+        img: react,
+        percentage: "90%",
+      },
+      {
+        id: "5",
+        name: "node",
+        img: node,
+        percentage: "70%",
+      },
+      {
+        id: "6",
+        name: "nextjs",
+        img: nextjs,
+        percentage: "70%",
+      },
+      {
+        id: "7",
+        name: "tailwind",
+        img: tailwind,
+        percentage: "90%",
+      },
+    ],
+    comfortable: [
+      {
+        id: "1",
+        name: "express",
+        img: express,
+        percentage: "80%",
+      },
+      {
+        id: "2",
+        name: "firebase",
+        img: firebase,
+        percentage: "90%",
+      },
+      {
+        id: "3",
+        name: "bootstrap",
+        img: bootstrap,
+        percentage: "90%",
+      },
+      {
+        id: "4",
+        name: "typescript",
+        img: typescript,
+        percentage: "70%",
+      },
+      {
+        id: "5",
+        name: "mongodb",
+        img: mongodb,
+        percentage: "70%",
+      },
+    ],
+    tools: [
+      {
+        id: "1",
+        name: "github",
+        img: "https://cdn-icons-png.flaticon.com/512/270/270798.png",
+        percentage: "90%",
+      },
+      {
+        id: "2",
+        name: "vscode",
+        img: vscode,
+        percentage: "90%",
+      },
+      {
+        id: "3",
+        name: "postman",
+        img: postman,
+        percentage: "90%",
+      },
+      {
+        id: "4",
+        name: "figma",
+        img: figma,
+        percentage: "90%",
+      },
+      {
+        id: "5",
+        name: "illustrator",
+        img: illustrator,
+        percentage: "90%",
+      },
+      {
+        id: "6",
+        name: "git",
+        img: git,
+        percentage: "90%",
+      },
+    ],
+  };
