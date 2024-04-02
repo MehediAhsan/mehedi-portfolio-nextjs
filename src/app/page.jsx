97d444602519dc6b963b87d7bbe177ac3c672357
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-04-02 20:31:03 +0600

    about page working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import About from "@/components/home/About";
 import Banner from "@/components/home/Banner";
 
 export default function Home() {
   return (
     <main className="">
       <Banner/>
+      <About/>
     </main>
   );
 }
