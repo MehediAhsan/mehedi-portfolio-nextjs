712d84489e41771f51a50f69fb2d91207d2056fa
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-11-15 21:27:26 +0600

    added modal

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -6,12 +6,12 @@ const Modal = ({ open, setOpen, children, title="Title" }) => {
     return (
         <>
             <div className={`${open ? 'bg-black fixed inset-0 z-40 opacity-30' : 'hidden'}`} onClick={() => setOpen(false)}></div>
-            <MotionWrapper variantName="fadeIn" delay={0.1} className={`${open ? 'fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' : 'hidden'}`}>
+            <MotionWrapper variantName="fadeIn" delay={0.1} className={`${open ? 'fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 md:w-6/12' : 'hidden'}`}>
                 <div className='flex justify-between items-center bg-primary px-5 py-3'>
                     <h1 className='uppercase font-semibold'>{title}</h1>
                     <ImCross onClick={() => setOpen(false)} className='cursor-pointer text-sm' />
                 </div>
-                <div className="overflow-y-auto bg-gray-200 py-8 px-4">{children}</div>
+                <div className="overflow-y-auto bg-gray-200 p-5 h-[350px] md:h-full">{children}</div>
             </MotionWrapper>
         </>
     );
